fbcd7f297cd64d1d998ee9d60d59a07011c66b71
ttwange <ttwange2@gmail.com>
2023-08-27 21:37:29 +0300

    Trying to find a working prefect style code

--- flows/trial.py
+++ flows/trial.py
@@ -2,42 +2,38 @@ import os
 import requests
 import zipfile
 from prefect import flow, task
+from datetime import timedelta
 
-@task()
-def download_zip_file(url, download_path):
-    os.makedirs(os.path.dirname(download_path), exist_ok=True)
+@task
+def download_and_extract():
+    zip_url = "https://divvy-tripdata.s3.amazonaws.com/202004-divvy-tripdata.zip"
+    zip_download_path = ".\data\202004-divvy-tripdata.zip"
+    zip_extracted_path = ".\data"
 
-    response = requests.get(url)
+    # Download the ZIP file
+    os.makedirs(os.path.dirname(zip_download_path), exist_ok=True)
+    response = requests.get(zip_url)
     
     if response.status_code == 200:
-        with open(download_path, 'wb') as file:
+        with open(zip_download_path, 'wb') as file:
             file.write(response.content)
-        return download_path
     else:
-        raise Exception(f"Failed to download the zip file from {url}.")
+        raise Exception(f"Failed to download the zip file from {zip_url}")
 
-@task()
-def extract_csv_from_zip(downloaded_path, zip_extracted_path):
-    with zipfile.ZipFile(downloaded_path, "r") as zip_ref:
+    # Unzip and save the CSV
+    with zipfile.ZipFile(zip_download_path, "r") as zip_ref:
         csv_file_name = [name for name in zip_ref.namelist() if name.endswith(".csv")]
         if csv_file_name:
             zip_ref.extract(csv_file_name[0], path=zip_extracted_path)
             csv_file_path = os.path.join(zip_extracted_path, csv_file_name[0])
-            print(f"CSV file extracted and saved: {csv_file_path}")
+            return csv_file_path
         else:
-            print("No CSV file found in the zip archive.")
-
+            return None
 
 @flow()
 def etl_web_to_gcs() -> None:
-# Define your tasks within the flow
-  period = 202004
-  zip_url = f"https://divvy-tripdata.s3.amazonaws.com/{period}-divvy-tripdata.zip"
-  zip_download_path = f".\data\{period}-divvy-tripdata.zip"
-  zip_extracted_path = ".\data"
-
-  downloaded_path = download_zip_file(zip_url, zip_download_path)
-  extract_csv_task = extract_csv_from_zip(downloaded_path, zip_extracted_path)
-
+    """The main ETL function"""
+    download_and_extract_result = download_and_extract()
+    
 if __name__ == '__main__':
     etl_web_to_gcs()
