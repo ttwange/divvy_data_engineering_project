26f20a8391d55e9a94bcec2c8b7ce84b4838e9db
ttwange <ttwange2@gmail.com>
2023-08-27 20:53:11 +0300

    Working on prefect extract script

--- flows/etl_web_to_gcs.py
+++ flows/etl_web_to_gcs.py
@@ -17,21 +17,25 @@ def download_zip_file(url, download_path):
         raise Exception(f"Failed to download the zip file from {url}.")
 
 @task()
-with zipfile.ZipFile(downloaded_path, "r") as zip_ref:
-    csv_file_name = [name for name in zip_ref.namelist() if name.endswith(".csv")]
-    if csv_file_name:
-        zip_ref.extract(csv_file_name[0], path=zip_extracted_path)
-        csv_file_path = os.path.join(zip_extracted_path, csv_file_name[0])
-        print(f"CSV file extracted and saved: {csv_file_path}")
-    else:
-        print("No CSV file found in the zip archive.")
+def extract_csv_from_zip(downloaded_path, zip_extracted_path):
+    with zipfile.ZipFile(downloaded_path, "r") as zip_ref:
+        csv_file_name = [name for name in zip_ref.namelist() if name.endswith(".csv")]
+        if csv_file_name:
+            zip_ref.extract(csv_file_name[0], path=zip_extracted_path)
+            csv_file_path = os.path.join(zip_extracted_path, csv_file_name[0])
+            print(f"CSV file extracted and saved: {csv_file_path}")
+        else:
+            print("No CSV file found in the zip archive.")
 
 @flow()
 def etl_web_to_gcs() -> None:
     period = 202004
     zip_url = f"https://divvy-tripdata.s3.amazonaws.com/{period}-divvy-tripdata.zip"
     zip_download_path = f".\data\{period}-divvy-tripdata.zip"
-    downloaded_path = download_zip_file(zip_url, zip_download_path)
     print(f"Zip file downloaded to: {downloaded_path}")
-
     zip_extracted_path = ".\data"
+    downloaded_path = download_zip_file(zip_url, zip_download_path)
+    csv_file_path = extract_csv_from_zip(downloaded_path, zip_extracted_path)
+
+if __name__ == '__main__':
+    etl_web_to_gcs()
