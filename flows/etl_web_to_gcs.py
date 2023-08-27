127f3d9dc4ae9b34eefbdbdc5f84cef8518bb40b
ttwange <ttwange2@gmail.com>
2023-08-27 20:43:20 +0300

    Working on prefect extract script

--- flows/etl_web_to_gcs.py
+++ flows/etl_web_to_gcs.py
@@ -1,23 +1,36 @@
-from pathlib import Path
-import pandas as pd
-from prefect import task,flow
-from prefect_gcp.cloud_storage import GcsBucket
+import os
+import requests
+import zipfile
+from prefect import flow, task
 
+@task(retries=3)
+def download_zip_file(url, download_path):
+    os.makedirs(os.path.dirname(download_path), exist_ok=True)
+
+    response = requests.get(url)
+    
+    if response.status_code == 200:
+        with open(download_path, 'wb') as file:
+            file.write(response.content)
+        return download_path
+    else:
+        raise Exception(f"Failed to download the zip file from {url}.")
 
 @task()
-def fetch(dataset: str) -> pd.DataFrame:
-    """Fetch a dataset from web to pandas dataframe"""
-    df = pd.read_csv(dataset)
-    return df
+zip_extracted_path = ".\data"
+with zipfile.ZipFile(downloaded_path, "r") as zip_ref:
+    csv_file_name = [name for name in zip_ref.namelist() if name.endswith(".csv")]
+    if csv_file_name:
+        zip_ref.extract(csv_file_name[0], path=zip_extracted_path)
+        csv_file_path = os.path.join(zip_extracted_path, csv_file_name[0])
+        print(f"CSV file extracted and saved: {csv_file_path}")
+    else:
+        print("No CSV file found in the zip archive.")
 
 @flow()
-def et_web_to_gcs() -> None:
-    """The main ETL function"""
+def etl_web_to_gcs() -> None:
     period = 202004
-    dataset = "https://data.cityofchicago.org/api/views/wrvz-psew/rows.csv?accessType=DOWNLOAD"
-    
-
-    df = fetch(dataset)
-
-if __name__ == "__main__":
-    et_web_to_gcs()
+    zip_url = f"https://divvy-tripdata.s3.amazonaws.com/{period}-divvy-tripdata.zip"
+    zip_download_path = f".\data\{period}-divvy-tripdata.zip"
+    downloaded_path = download_zip_file(zip_url, zip_download_path)
+    print(f"Zip file downloaded to: {downloaded_path}")
