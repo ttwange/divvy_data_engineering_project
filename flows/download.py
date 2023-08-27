632501903f2564408741234144c103232b68b9ef
ttwange <ttwange2@gmail.com>
2023-08-27 20:04:50 +0300

    Made some small changes to ensure that we only download and save the csv file

--- flows/download.py
+++ flows/download.py
@@ -3,17 +3,6 @@ import requests
 import zipfile
 
 def download_zip_file(url, download_path):
-    """
-    Download a zip file from the given URL.
-
-    Args:
-        url (str): The URL of the zip file.
-        download_path (str): The full path where the downloaded zip file will be saved.
-
-    Returns:
-        str: The path to the downloaded zip file.
-    """
-    # Create the download directory if it doesn't exist
     os.makedirs(os.path.dirname(download_path), exist_ok=True)
 
     response = requests.get(url)
@@ -25,22 +14,18 @@ def download_zip_file(url, download_path):
     else:
         raise Exception(f"Failed to download the zip file from {url}.")
 
-# Example usage
 zip_url = "https://divvy-tripdata.s3.amazonaws.com/202004-divvy-tripdata.zip"
-zip_download_path = ".\divvy\data\202004-divvy-tripdata.zip"
+zip_download_path = ".\data\202004-divvy-tripdata.zip"
 downloaded_path = download_zip_file(zip_url, zip_download_path)
 print(f"Zip file downloaded to: {downloaded_path}")
 
-# Unzip the downloaded file and save the CSV
-zip_extracted_path = ".\divvy\data"
+# Unzip the downloaded file and save only the CSV
+zip_extracted_path = ".\data"
 with zipfile.ZipFile(downloaded_path, "r") as zip_ref:
-    zip_ref.extractall(zip_extracted_path)
-    print("Zip file extracted.")
-
-# Assuming the CSV file name follows a specific pattern, find it
-csv_file_name = [name for name in os.listdir(zip_extracted_path) if name.endswith(".csv")]
-if csv_file_name:
-    csv_file_path = os.path.join(zip_extracted_path, csv_file_name[0])
-    print(f"CSV file found: {csv_file_path}")
-else:
-    print("No CSV file found in the extracted folder.")
+    csv_file_name = [name for name in zip_ref.namelist() if name.endswith(".csv")]
+    if csv_file_name:
+        zip_ref.extract(csv_file_name[0], path=zip_extracted_path)
+        csv_file_path = os.path.join(zip_extracted_path, csv_file_name[0])
+        print(f"CSV file extracted and saved: {csv_file_path}")
+    else:
+        print("No CSV file found in the zip archive.")
